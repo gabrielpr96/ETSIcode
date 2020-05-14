39a35a0737a942e7843ccdd2d005d21cf112ecb8
Borja Live <borjainlive@gmail.com>
2020-05-14 12:24:56 +0200

    Version 2 Canal panama

---
+++ Segundo/APSO/Programacion/Canal de panama/V2/barcooeste.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <signal.h>
+#include <time.h>
+#include "comunbarcos.h"
+
+int main(){
+	srand(time(0));
+	struct Parametros param;
+	key_t clave;
+	int cola;
+	signal(10,R10);
+	signal(12,R12);
+
+	clave = ftok ("./fichcola.txt", 18);
+    cola = crea_cola(clave);
+
+	read(2, (char*)&param, sizeof(struct Parametros));
+	close(2);
+	dup(1);
+
+	visualiza(cola, VOESTEIN, PINTAR, TIPOOESTE);
+	sleep((rand()%(param.lagomax-param.lagomin+1))+param.lagomin);
+	visualiza(cola, VOESTEIN, BORRAR, TIPOOESTE);
+
+
+	return 0;
+}
