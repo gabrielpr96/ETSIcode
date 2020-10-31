7a4d9234a8a578f1859642df130e1b354a344101
BorjaLive <borjainlive@gmail.com>
2020-10-31 21:07:34 +0100

    Mejora practica 2 DDSI

--- Tercero/DDSI/Practica 2/src/Aplicacion/aplicacionCliente.java
+++ Tercero/DDSI/Practica 2/src/Aplicacion/aplicacionCliente.java
@@ -4,6 +4,8 @@ import Persistencia.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class aplicacionCliente {
 
@@ -47,7 +49,7 @@ public class aplicacionCliente {
         Scanner sc = new Scanner(System.in);
         manejaExperto me = new manejaExperto(co);
         System.out.print("Pais: ");
-        String pais = sc.next();
+        String pais = sc.nextLine();
         ArrayList<experto> expertos;
         try {
             expertos = me.listaExpertosPorPais(pais);
@@ -65,15 +67,15 @@ public class aplicacionCliente {
         manejaExperto me = new manejaExperto(co);
         experto e = new experto();
         System.out.print("Codigo: ");
-        e.setCodExperto(sc.next());
+        e.setCodExperto(sc.nextLine());
         System.out.print("Nombre: ");
-        e.setNombre(sc.next());
+        e.setNombre(sc.nextLine());
         System.out.print("Pais: ");
-        e.setPais(sc.next());
+        e.setPais(sc.nextLine());
         System.out.print("Sexo: ");
-        e.setSexo(sc.next());
+        e.setSexo(sc.nextLine());
         System.out.print("Especialidad: ");
-        e.setEspecialidad(sc.next());
+        e.setEspecialidad(sc.nextLine());
         try {
             me.insertaExperto(e);
             System.out.println("Experto insertado correctamente");
@@ -85,19 +87,53 @@ public class aplicacionCliente {
     public static void ejercicio3() {
         Scanner sc = new Scanner(System.in);
         manejaColabora mc = new manejaColabora(co);
+        manejaExperto me = new manejaExperto(co);
+        manejaCaso ma = new manejaCaso(co);
         colabora c = new colabora();
         System.out.print("Codigo experto: ");
-        c.setCodExperto(sc.next());
+        c.setCodExperto(sc.nextLine());
         System.out.print("Codigo caso: ");
-        c.setCodcaso(sc.next());
+        c.setCodcaso(sc.nextLine());
         System.out.print("Fecha: ");
-        c.setFecha(sc.next());
+        c.setFecha(sc.nextLine());
         System.out.print("Descripcion: ");
-        c.setDescripcionColaboracion(sc.next());
+        c.setDescripcionColaboracion(sc.nextLine());
         try {
+            co.inicioTransaccion();
+            if(!me.existeExperto(c.getCodExperto())){
+                experto e = new experto();
+                System.out.println("No existe ningun experto con el codigo: "+c.getCodExperto()+", introduzca sus datos.");
+                e.setCodExperto(c.getCodExperto());
+                System.out.print("Nombre: ");
+                e.setNombre(sc.nextLine());
+                System.out.print("Pais: ");
+                e.setPais(sc.nextLine());
+                System.out.print("Sexo: ");
+                e.setSexo(sc.nextLine());
+                System.out.print("Especialidad: ");
+                e.setEspecialidad(sc.nextLine());
+                me.insertaExperto(e);
+            }
+            if(!ma.existeCaso(c.getCodCaso())){
+                caso a = new caso();
+                System.out.println("No existe ningun caso con el codigo: "+c.getCodCaso()+", introduzca sus datos.");
+                a.setCodCaso(c.getCodCaso());
+                System.out.print("Nombre: ");
+                a.setNombre(sc.nextLine());
+                System.out.print("Fecha Inicio: ");
+                a.setFechaInicio(sc.nextLine());
+                System.out.print("Fecha Fin: ");
+                a.setFechaFin(sc.nextLine());
+                ma.insertaCaso(a);
+            }
             mc.insertaColaboracion(c);
             System.out.println("Colaboracion insertada correctamente");
         } catch (SQLException ex) {
+            try {
+                co.finTransaccionRollback();
+            } catch (SQLException ex1) {
+                System.out.println("No se ha podido cancelar la transaccion\n"+ex1.getMessage());
+            }
             System.out.println("No se ha podido insertar la colaboracion\n"+ex.getMessage());
         }
     }
