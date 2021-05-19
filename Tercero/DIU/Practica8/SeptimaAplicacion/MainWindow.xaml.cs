9418e5eebc85796b62073ede7931a4e5ad8da6e1
Borja López Pineda <borjainlive@gmail.com>
2021-05-19 14:00:52 +0200

    Practica 8 Terminada DIU

---
+++ Tercero/DIU/Practica8/SeptimaAplicacion/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SeptimaAplicacion {
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void onEnviar(object sender, RoutedEventArgs e) {
+            Button boton = sender as Button;
+            if(boton != null) {
+                Window w;
+                if(Pass.Password.Equals("Interfaces")) {
+                    w = new Profesores();
+                } else {
+                    w = new Alumnos();
+                }
+                w.ShowDialog();
+            }
+        }
+    }
+}
---
+++ Tercero/DIU/Practica8/SeptimaAplicacion/Profesores.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SeptimaAplicacion {
+    /// <summary>
+    /// Lógica de interacción para Profesores.xaml
+    /// </summary>
+    public partial class Profesores : Window {
+        public Profesores() {
+            InitializeComponent();
+        }
+
+        private void onAbrirWeb(object sender, RoutedEventArgs e) {
+            MenuItem menuItem = sender as MenuItem;
+            if(menuItem != null) {
+                System.Diagnostics.Process.Start(menuItem.Tag.ToString());
+            }
+        }
+    }
+}
