2cd2a2d199868e9f99c7d676c94826d0f3e9abbb
BorjaLive <borjainlive@gmail.com>
2021-10-13 08:17:42 +0200

    Practicas jueves

--- 4 Cuarto/R/Sesiones/Semana 2/script3.m
+++ 4 Cuarto/R/Sesiones/Semana 2/script3.m
@@ -1,10 +1,18 @@
 k = 1;
+
+for s = 0 : 0.01 : 2*pi
+    t(k) = s;
+    y(k) = signal_v0(t(k));
+    k = k + 1;
+end
+
+sUltimo = s
 tstart = tic();
 telapsed = 0;
 
 while telapsed < 10
     telapsed = toc(tstart);
-    t(k) = telapsed;
+    t(k) = sUltimo+telapsed;
     y(k) = signal_v0(t(k));
     k = k + 1;
 end
