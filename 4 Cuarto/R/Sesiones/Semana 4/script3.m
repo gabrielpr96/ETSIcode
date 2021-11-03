9c50f029da77fd0dbebdfff6bbad3051c3561e93
BorjaLive <borjainlive@gmail.com>
2021-11-03 12:44:57 +0100

    Mejor pinta robot

--- 4 Cuarto/R/Sesiones/Semana 4/script3.m
+++ 4 Cuarto/R/Sesiones/Semana 4/script3.m
@@ -5,6 +5,6 @@ tStart = tic();
 tElapsed = 0;
 while tElapsed < 2*pi
     tElapsed = toc(tStart);
-    pinta_robot(sin(tElapsed)*5, -5, tElapsed, 0);
+    pinta_robot(sin(tElapsed)*5, -5, 0, tElapsed);
 end
 
